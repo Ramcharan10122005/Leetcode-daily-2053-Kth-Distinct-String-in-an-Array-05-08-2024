b3de85cd54f71ebaa91d39a51d1348f3a73071e0
Ramcharan Kundena <160163532+Ramcharan10122005@users.noreply.github.com>
2024-08-05 14:20:44 +0530

    Create Code2.cpp

---
+++ Code2.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string kthDistinct(vector<string>& arr, int k) {
+        map<string, int> mp;
+        for (auto s : arr) {
+            mp[s]++;
+        }
+        for (auto s : arr) {
+            if (mp[s] == 1)
+                k--;
+            if (k == 0)
+                return s;
+        }
+        return "";
+    }
+};
